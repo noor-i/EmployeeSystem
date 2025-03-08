66a79bad97c3de924c67e1c4692c0f3802a8af09
mia16 <mahnur.imran@gmail.com>
2025-03-08 13:56:09 -0800

    enabled strict mode for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
